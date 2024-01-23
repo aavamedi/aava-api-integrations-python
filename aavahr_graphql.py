c4a570d63fe6c8999ec171d366fe49420b076a08
Jyri Jokinen <jyri.jokinen@aava.fi>
2024-01-23 12:21:47 +0200

    Cleaner organizing of import functions

--- aavahr_graphql.py
+++ aavahr_graphql.py
@@ -1,8 +1,28 @@
 import json
-from urllib import request
+import logging
+
+from urllib import request, error
+
+
+def capfirst(text):
+    """
+    Only changes the case of first letter of string to capital,
+    leaves others untouched, unlike capitalize()
+    """
+    return text[:1].upper() + text[1:]
 
 
 def graphql_request(parameters: dict, payload: str) -> dict:
+    """
+    Performs the actual GraphQL request to Aava-API.
+
+    Args:
+        parameters (dict): Contains URL and credential information for API connection
+        payload (str): A formatted GraphQL request
+
+    Returns:
+        dict: _description_
+    """
     url = parameters["aavaApiServer"] + "/hr"
     req = request.Request(url, data=payload.encode("utf-8"), method="POST")
     req.add_header(
@@ -10,149 +30,93 @@ def graphql_request(parameters: dict, payload: str) -> dict:
     )
     req.add_header("Accept", "application/json")
     req.add_header("Content-Type", "application/json")
-    response = request.urlopen(req)
-    data = json.loads(response.read().decode("utf-8"))["data"]
-    return data
+    try:
+        response = request.urlopen(req)
+        result = json.loads(response.read().decode("utf-8"))
+
+        if "errors" in result:
+            error_messages = []
+            for err in result["errors"]:
+                locations = []
+                for loc in err["locations"]:
+                    locations.append(f"row {loc['line']}, column {loc['column']}")
+                error_messages.append(f"{err['message']}, {' & '.join(locations)}")
+            raise ValueError("\n".join(error_messages))
+
+        return result["data"]
+    except error.HTTPError:
+        logging.critical(f"Error importing {type}s. Exiting.")
+        exit()
+    except ValueError as e:
+        logging.error("Invalid content %s", e)
+        return None
+
+
+def format_query(type: str) -> str:
+    """
+    Format a mutation that can be sent to Aava-API
+
+    Args:
+        type (str): The type of import (department, costCenter, employee, absence)
+
+    Returns:
+        str: The formatted query string that can be passed to Aava-API
+    """
+
+    query = f"""
+            mutation import{capfirst(type)}s(
+                $organizationExternalId: ID!
+                ${type}s: [{capfirst(type)}Input!]!
+            ) {{
+                import{capfirst(type)}s(
+                organizationExternalId: $organizationExternalId
+                {type}s: ${type}s
+                ) {{
+                    messageId
+                }}
+            }}
+        """
+
+    return query
+
+
+def import_data(type: str, parameters: dict, data: dict) -> dict:
+    """
+    Performs the import query of a given type.
+
+    Args:
+        type (str): Type of import (department, costCenter, employee, absence)
+        parameters (dict): URL and credentials for Aava-API
+        data (dict): The data that is to be imported
+
+    Returns:
+        dict: _description_
+    """
+    request_data = {
+        "query": format_query(type),
+        "variables": {
+            "organizationExternalId": parameters["organizationId"],
+            f"{type}s": data,
+        },
+    }
+    result = graphql_request(parameters=parameters, payload=json.dumps(request_data))
+    return result
 
 
 def import_departments(parameters: dict, departments: dict) -> dict:
-    """
-    Imports the department information to Aava API.
-
-    Args:
-        parameters (dict): Parameters for connecting to Aava API (see properties-template.json)
-        departments (dict): A dictionary object containing the department data (see simple_example_hrm.py)
-
-    Returns:
-        dict: Structure containing the request ID for querying the status of request; in r['importDepartments']['messageId']
-    """
-
-    request_data = {
-        "query": """
-            mutation importDepartments(
-                $organizationExternalId: ID!
-                $departments: [DepartmentInput!]!
-            ) {
-                importDepartments(
-                organizationExternalId: $organizationExternalId
-                departments: $departments
-                ) {
-                messageId
-                }
-            }
-        """,
-        "variables": {
-            "organizationExternalId": parameters["organizationId"],
-            "departments": departments,
-        },
-    }
-
-    return graphql_request(parameters, json.dumps(request_data))
+    return import_data("department", parameters, departments)
 
 
 def import_cost_centers(parameters: dict, costCenters: dict) -> dict:
-    """
-    Imports the cost center information to Aava API.
-
-    Args:
-        parameters (dict): Parameters for connecting to Aava API (see properties-template.json)
-        costCenters (dict): A dictionary object containing the cost center data (similar to department data)
-
-    Returns:
-        dict: Structure containing the request ID for querying the status of request; in r['importCostCenters']['messageId']
-    """
-
-    request_data = {
-        "query": """
-            mutation importCostCenters(
-                $organizationExternalId: ID!
-                $costCenters: [CostCenterInput!]!
-            ) {
-                importCostCenters(
-                organizationExternalId: $organizationExternalId
-                costCenters: $costCenters
-                ) {
-                messageId
-                }
-            }
-        """,
-        "variables": {
-            "organizationExternalId": parameters["organizationId"],
-            "costCenters": costCenters,
-        },
-    }
-
-    return graphql_request(parameters, json.dumps(request_data))
+    return import_data("costCenter", parameters, costCenters)
 
 
 def import_employees(parameters: dict, employees: dict) -> dict:
-    """
-    Imports the employee information retrieved from HRM to Aava API
-
-    Args:
-        parameters (dict): Parameters for connecting to Aava API (see properties-template.json)
-        employees (dict): A dictionary object containing the employee data (see simple_example_hrm.py)
-
-    Returns:
-        dict: Structure containing the request ID for querying the status of request; in r['importEmployees']['messageId']
-    """
-
-    request_data = {
-        "query": """
-            mutation importEmployees(
-                $organizationExternalId: ID!
-                $employees: [EmployeeInput!]!
-            ) {
-                importEmployees(
-                    organizationExternalId: $organizationExternalId
-                    employees: $employees
-                ) {
-                messageId
-                }
-            }
-        """,
-        "variables": {
-            "organizationExternalId": parameters["organizationId"],
-            "employees": employees,
-        },
-    }
-
-    return graphql_request(parameters, json.dumps(request_data))
+    return import_data("employee", parameters, employees)
 
 
 def import_absences(parameters: dict, absences) -> dict:
-    """
-    Imports the absence data retrieved from work hour tracking system to Aava API
-
-    Args:
-        parameters (dict): Parameters for connecting to Aava API (see properties-template.json)
-        absences (dict): A dictionary object containing the absence data (see simple_example_time_tracker.py)
-
-    Returns:
-        dict: Structure containing the request ID for querying the status of request; in r['importAbsences']['messageId']
-    """
-
-    request_data = {
-        "query": """
-            mutation importAbsences(
-                $organizationExternalId: ID!
-                $absences: [AbsenceInput!]!
-            ) {
-                importAbsences(
-                    organizationExternalId: $organizationExternalId
-                    absences: $absences
-                ) {
-                    messageId
-                }
-            }
-        """,
-        "variables": {
-            "organizationExternalId": parameters["organizationId"],
-            "absences": absences,
-        },
-    }
-
-    return graphql_request(parameters, json.dumps(request_data))
+    return import_data("absence", parameters, absences)
 
 
 def get_statuses(parameters: dict, message_ids: list) -> dict:
